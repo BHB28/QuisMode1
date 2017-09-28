0e7cfb015d084a36e2839e642568829c22351edd
BHB28 <bdschneider90@gmail.com>
2017-09-28 22:06:23 +0000

    lesson 28

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ApplicationRecord
   belongs_to :user
   validates :name, :address, :description, presence: true
-  validates :name, length: {minimum: 2}
+  validates :name, length: {minimum: 4}
 end
