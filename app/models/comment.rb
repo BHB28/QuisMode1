98bce094b57d81a4c7f1f2a226e4cfd16981164b
BHB28 <bdschneider90@gmail.com>
2017-10-02 21:29:13 +0000

    lesson 35test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+  RATINGS = {
+    'One star': '1_star',
+    'Two stars': '2_stars',
+    'Three stars': '3_stars',
+    'Four stars': '4_stars',
+    'Five stars': '5_stars'
+  }
 end
